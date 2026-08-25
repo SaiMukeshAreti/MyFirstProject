85a0ac8b59686dbf6f7b2437d07ad812d43da976
SaiMukeshAreti <saimukeshareti99@gmail.com>
2026-08-25 23:51:50 +0530

    Added ForEachLoop

--- ForLoop.java
+++ ForLoop.java
@@ -2,10 +2,17 @@ package com.forloop;
 
 public class ForLoop {
 	public static void main(String[] args) {
+
+		
 		
 		for(int i=1;i<=5;i++) {
 			System.out.println("SaiMukesh Areti");
 			
+		}
+
+		int[] numbers = {1, 2, 3, 4, 5};
+		for(int number : numbers) {
+			System.out.println(number);
 		}
 	}
 
